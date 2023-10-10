cf349e079aa5485c25b2b81b62dad95b8456683e
pintongc <pintongc@nycstudents.net>
2023-10-10 09:26:10 -0400

    2

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const a = prompt("Please enter your name.")
-const e = prompt("Please enter your height.(cm)", "0");
-const f = prompt("Please enter your weight.(kg)", "0");
+const a = prompt("Please enter your name.");
+const e = prompt("Please enter your height(cm)", "0");
+const f = prompt("Please enter your weight(kg)", "0");
 
 const c = e * e;
 const b = f/c;
-const bmi = Math.round(b * 10)/10;
+const bmi = Math.round((b * 10000) * 10)/10;
 
 function computeBMI() {
     
