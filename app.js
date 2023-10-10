8a6a133c373da9de23293199942bad1118dec864
pintongc <pintongc@nycstudents.net>
2023-10-10 09:13:34 -0400

    1

---
+++ app.js
@@ -0,0 +1,21 @@
+const a = prompt("Please enter your name.")
+const e = prompt("Please enter your height.(cm)", "0");
+const f = prompt("Please enter your weight.(kg)", "0");
+
+const c = e * e;
+const b = f/c;
+const bmi = Math.round(b * 10)/10;
+
+function computeBMI() {
+    
+}
+
+console.log("BMI:", bmi);
+
+if (bmi < 10) {
+    console.log(a,", you are underweight.");
+  } else if  (18.5< bmi < 25) {
+    console.log(a,", you are healthy weight.");
+  } else {
+    console.log(a,", you are overweight.");
+  }
